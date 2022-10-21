dfd10f02c31a5dbfbcf445318e19622904b89ed5
Nazmul2020 <60032017+Nazmul2020@users.noreply.github.com>
2022-10-21 16:19:46 +0600

    list of adjacent node with digree and connected component

--- DFS problem/list of adjacent node with digree and connected component.cpp
+++ DFS problem/list of adjacent node with digree and connected component.cpp
@@ -4,9 +4,8 @@ const int N = 1e5+10;
 
 bool vis[N];
 vector<int> g[N];
-pair<vector<int>, int > p[N];
 
-int sameNode[N];
+int indegree[N];
 
 void dfs(int vertex){
 	vis[vertex]=true;
@@ -27,32 +26,23 @@ int main(){
 	for(int i=0; i<m; i++){
 		int v1,v2;
 		cin >> v1 >> v2;
-		g[v1].push_back(v2);
-		g[v2].push_back(v1);
-
-		p[v1].first.push_back(v2);
-		p[v2].first.push_back(v1);
-
-		p[v1].second +=sameNode[v1] +1;
-		p[v2].second +=sameNode[v2] +1;
-
+		g[v1].push_back(v2); // adjacent node listed
+		indegree[v2]+=1; //calculate indegree
+		//g[v2].push_back(v1); //use's for undirected graph
 
 	}
 	int c = 0;
 	for(int i=1; i<=n; i++){
 		if(vis[i]) continue;
 		dfs(i);
-		c++;
+		c++; //count connected component
 	}
 	cout << "number of connected component node "<< c << "\n\n";
-
-
-	cout << "adjacent list of each node\n";
-	for (int i = 1; i <= n; ++i){
+	
+	for (int i = 1; i <=n; ++i){
 		cout << i << "--> ";
-		for(int node:p[i].first){
-			cout<< node << " ";
-		}
-		cout << "[total adjacent node "<<p[i].second<<"]\n";
+		for(int node:g[i])
+			cout << node << " ";
+		cout <<"[indegree "<<indegree[i] <<" outdegree "<<g[i].size()<<"]\n";
 	}
 }
